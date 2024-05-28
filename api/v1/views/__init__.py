db697e64d8df21f00b594153ae3dc0600ca523cc
BousfihaHassan <bousfihahassan2019@gmail.com>
2024-05-28 02:46:40 +0000

    a

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -11,5 +11,8 @@ from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
+<<<<<<< HEAD
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+=======
+>>>>>>> parent of 6622f05... Place
